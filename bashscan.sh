fe2a2ad4d9bec9caedce72a8af717d0c448a0ea5
keechy1231 <76797169+keechy1231@users.noreply.github.com>
2023-04-19 22:16:50 +0100

    Update bashscan.sh
    Make it go brrrrrrr

--- bashscan.sh
+++ bashscan.sh
@@ -6,7 +6,34 @@ if [ "$#" -ne 1 ]; then
 fi
 
 target="$1"
+open_ports=""
 
+# Define function to scan ports
+scan_port() {
+    local port="$1"
+    if timeout 1 bash -c "echo >/dev/tcp/$target/$port" >/dev/null 2>&1; then
+        if ! echo "$open_ports" | grep -q "\b$port/tcp\b"; then
+            open_ports+=" $port/tcp"
+            echo -e "\rScanning port $port... \033[0;32m[OPEN]\033[0m"
+        fi
+    fi
+}
+
+# Launch threads to scan ports in parallel
 for port in $(seq 1 65535); do
-    timeout 1 bash -c "echo >/dev/tcp/$target/$port" >/dev/null 2>&1 && echo "$port/tcp open"
+    # Limit number of threads to 10
+    while [ $(jobs | wc -l) -ge 10 ]; do
+        sleep 0.1
+    done
+    scan_port "$port" &
 done
+
+# Wait for all threads to finish
+wait
+
+if [ -n "$open_ports" ]; then
+    echo -e "\n\nOpen ports found:"
+    echo "$open_ports"
+else
+    echo -e "\n\nNo open ports found."
+fi
